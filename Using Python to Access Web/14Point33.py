b84ce1e4549212627d8f4a127bead016c915dd39
Matsobane <bmokomane@gmail.com>
2020-03-15 00:07:44 +0200

    Location Pin Point

---
+++ Using Python to Access Web/14Point33.py
@@ -0,0 +1,28 @@
+import urllib
+import json
+import urllib.request
+serviceurl = "http://py4e-data.dr-chuck.net/json?"
+
+while True:
+	address = input("Enter location:")
+	if len(address) <1: break
+
+	url = serviceurl+ urllib.parse.urlencode ({"sensor": "false,", "address":address})
+	print("retrieving", url)
+	uh = urllib.request.urlopen(url)
+	data = uh.read()
+	print("retrieved", len(data), "characters")
+	try: js=json.loads(str(data))
+	except: js= None
+	if 'status' not in js or js['status'] != 'OK':
+	       print("=== failure to retrieve ===")
+	       print(data)
+	continue
+
+	print(json.dumps(js, indent = 4))
+
+	lat = js["results"] [0] ["geometry"] ["location"]["lat"]
+	lng = js["results"] [0] ["geometry"] ["location"]["lng"]
+	print("lat", lat, "lng", lng)
+	location = js["results"][0] ["formatted_address"]
+	print(location)
