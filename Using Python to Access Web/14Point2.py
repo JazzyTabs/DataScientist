dd1cf92fa077959ec44d33f5e3284632b973b7da
Matsobane <bmokomane@gmail.com>
2020-03-14 23:35:04 +0200

    Json requests urllib

---
+++ Using Python to Access Web/14Point2.py
@@ -0,0 +1,16 @@
+import json
+import urllib
+import urllib.request
+count = 0
+
+url = "http://py4e-data.dr-chuck.net/comments_358889.json"
+print("retrieving URL. Stand by.")
+uh = urllib.request.urlopen(url)
+data= uh.read()
+
+info = json.loads(data)
+for item in info["comments"]:
+	#print item["count"]
+	number = int(item["count"])
+	count = count + number
+print(count)
