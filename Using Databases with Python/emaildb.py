2612c7c10e09e835e9ac84ed0a82dd142c853f05
Matsobane Mokomane <matsobanemokomane@Matsobanes-MacBook-Pro.local>
2020-04-05 17:53:14 +0200

    Python and MySQL

--- Using Databases with Python/emaildb.py
+++ Using Databases with Python/emaildb.py
@@ -1,38 +1,62 @@
-import sqlite3
+'''
+Author: Matsobane Mokomane
+Language: Python and SQL
+Date: April 05th 2020
+'''
 
+
+import sqlite3
+import urllib
+import urllib.request
+
+#Connecting to the file in which we want to store our db
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
 
-cur.execute('''
-DROP TABLE IF EXISTS Counts''')
+#Deleting any possible table that may affect this assignment
+cur.execute('''DROP TABLE IF EXISTS Counts''')
 
-cur.execute('''
-CREATE TABLE Counts (org TEXT, count INTEGER)''')
+#Creating the table we're going to use
+cur.execute('''CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+#Indicating the file (URL in this case) from where we'll read the data
+
 
 fname = input('Enter file name: ')
 if ( len(fname) < 1 ) : fname = 'mbox.txt'
 fh = open(fname)
+
+#Reading each line of the file
 for line in fh:
+
+    #Finding an email address and splitting it into name and organization
     if not line.startswith('From: ') : continue
     pieces = line.split()
     email = pieces[1]
-    parts = email.split('@')
-    org = parts[-1]
-    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org, ))
+    (emailname, organization) = email.split("@")
+    print(email)
+
+    #Updating the table with the correspondent information
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (organization, ))
     row = cur.fetchone()
     if row is None:
         cur.execute('''INSERT INTO Counts (org, count)
-                VALUES ( ?, 1 )''', ( org, ) )
+                VALUES ( ?, 1 )''', ( organization, ) )
     else :
         cur.execute('UPDATE Counts SET count=count+1 WHERE org = ?',
-            (org, ))
-    conn.commit()
+            (organization, ))
 
+# We commit the changes after they've finished because this speeds up the
+# execution and, since our operations are not critical, a loss wouldn't suppose
+# any problem
+conn.commit()
+
+# Getting the top 10 results and showing them
 sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
-
-print
-print "Counts:"
+#print
+print("Counts:")
 for row in cur.execute(sqlstr) :
     print(str(row[0]), row[1])
 
+#Closing the DB
 cur.close()
