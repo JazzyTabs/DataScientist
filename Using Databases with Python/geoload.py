a3501e43ace2a1ff3c6f5528ffc5db0aaca819df
MatsobaneMokomane <bmokomane@gmail.com>
2020-04-28 00:09:28 +0200

    fixed location and db

--- Using Databases with Python/geoload.py
+++ Using Databases with Python/geoload.py
@@ -5,7 +5,6 @@ import json
 import time
 import ssl
 import sys
-import os
 
 api_key = False
 # If you have a Google Places API key, enter it here
@@ -29,7 +28,7 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Locations (address TEXT, geodata TEXT)
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-# Opening the where.data file and looping within the data file so to retrive over less than 200 locations if greater than 200 the loop will end
+
 fh = open("where.data")
 count = 0
 for line in fh:
@@ -77,5 +76,5 @@ for line in fh:
     if count % 10 == 0 :
         print('Pausing for a bit...')
         time.sleep(5)
-# Displaying
+
 print("Run geodump.py to read the data from the database so you can vizualize it on a map.")
